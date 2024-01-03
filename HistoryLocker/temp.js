59dee880d2ba09ab8d9d0f8cb1f6e06d4a272297
elangoldman1 <113951112+elangoldman1@users.noreply.github.com>
2024-01-02 23:45:50 -0500

    manifest, html and js files for extension

---
+++ HistoryLocker/temp.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const redirectButton = document.getElementById('unlockHistory');
+
+    redirectButton.addEventListener('click', () => {
+        // Replace the URL below with the desired destination URL
+        window.location.href = 'https://www.youtube.com/watch?v=xm3YgoEiEDc';
+    });
+});
